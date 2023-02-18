d3d89c8ce802b1f448f93540b1aa0f72b715bdce
Yorkey <yakubgawa@gmail.com>
2023-02-18 06:11:36 -0500

    4-puts.c

---
+++ 0x00-hello_world/yorkey.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+	char f,m,l;
+	int age;
+	scanf(" %c %c %c %d", &f, &m, &l, &age);
+	printf(" There is a man called: %c%c%c and is about %dft tall"\n,'f','m','l', age);
+	return(0);
+}
