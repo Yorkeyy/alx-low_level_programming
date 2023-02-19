ab3a722881261085ebdbb520fdeccd6bf43de762
Yorkey <yakubgawa@gmail.com>
2023-02-19 08:03:39 -0500

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	printf("size of int: %lu byte(s)\n", sizeof(int));
 	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("size of a float: %fbyte(s)\n", sizeof(float));
+	printf("size of a float: %lubyte(s)\n", sizeof(float));
 	return (0);
 
 }
