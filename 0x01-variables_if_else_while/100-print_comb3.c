cf653158cef2cc0e3540e48f1ea157b9b71d95f5
YUSUF <yakubgawa@gmail.com>
2023-02-20 19:23:51 -0800

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the numbers from 00 to 99, numbers separated by
+ * main - Prints the numbers from 01 to 89, numbers separated by
  *        a comma followed by a space, in ascending order.
  *
  * Return: Always 0.
@@ -10,12 +10,12 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 10; digit1++)
+	for (digit1 = 1; digit1 < 10; digit1++)
 	{
-		for (digit2 = 0; digit2 < 10; digit2++)
+		for (digit2 = 1; digit2 < 10; digit2++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+			putchar((digit1 % 10) + '1');
+			putchar((digit2 % 10) + '1');
 
 			if (digit1 == 9 && digit2 == 9)
 				continue;
